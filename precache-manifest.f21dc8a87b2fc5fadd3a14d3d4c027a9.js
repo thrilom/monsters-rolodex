b73b424125d242f654762ccfe44d6043de0adfee
Joe Stephens <bodynsoulgraphix@gmail.com>
2020-07-29 08:41:04 -0500

    Updates

---
+++ precache-manifest.f21dc8a87b2fc5fadd3a14d3d4c027a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d40a56ac93d8679935aa690fa1f5787",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "4c8336934e311b0f71d3",
+    "url": "/monsters-rolodex/static/css/main.c0338e95.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c8336934e311b0f71d3",
+    "url": "/monsters-rolodex/static/js/main.b533bda2.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
