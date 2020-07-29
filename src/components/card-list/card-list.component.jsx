6513f2e73e385491dd36a500473ea78a03d5a4ca
Joe Stephens <bodynsoulgraphix@gmail.com>
2020-07-29 08:41:37 -0500

    adding commit message

---
+++ src/components/card-list/card-list.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Card } from "../card/card.component";
+
+import "./card-list.component.scss";
+
+export const CardList = props => (
+      <div className='card-list'>
+        {props.monsters.map(monster => (
+          <Card key={monster.id} monster={monster} />
+        ))}
+      </div>
+    );
